526d59256c3d614b221a4e184fd72784def83ee0
Kévin Joya <kevin.joya@outlook.com>
2023-11-30 22:51:15 +0100

    [feat/init-storybook] Created pain perdu files

---
+++ stories/PainPerdu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const PainPerdu = () => {
+  const [count, setCount] = React.useState(0);
+
+	function incrementCount() {
+		setCount(count + 1)
+		console.log("🚀 ~ file: PainPerdu.jsx:8 ~ incrementCount ~ count:", count)
+	}
+
+  return (
+		<section>
+			<h1> Ca part sur du pain perdu </h1>
+			<button
+				style={{ marginLeft: '10%' }}
+				onClick={incrementCount}
+			>
+				Go +1
+			</button>
+			<div style={{ textAlign: 'center' }}>
+				{ count }
+			</div>
+		</section>
+  );
+};
---
+++ stories/PainPerdu.stories.js
@@ -0,0 +1,16 @@
+import { PainPerdu } from './PainPerdu';
+
+export default {
+  title: 'Example/PainPerdu',
+  component: PainPerdu,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+  export const CmdK = {
+    args: {
+      primary: true,
+      label: 'Button',
+    },
+  };
