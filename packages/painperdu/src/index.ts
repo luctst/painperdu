3ef78c7c4e86eb9eb25fb1b98852fb6dc5cd06e7
Kévin <kevin.joya@outlook.com>
2023-12-04 15:34:18 +0100

    Feat/init storybook (#2)
    Co-authored-by: luctst <lucas.tostee@gmail.com>

--- packages/painperdu/src/index.ts
+++ packages/painperdu/src/index.ts
@@ -1,18 +1,1 @@
 console.log('coucou')
-
-import { useState } from 'react'
-
-function Test() {
-	const [ state, setState ] = useState(0)
-
-	return (
-		<>
-			<div>
-				<h1> Count: {state} </h1>
-				<button onclick={ setState(state + 1) }> Click to add </button>
-			</div>
-		</>
-	);
-}
-
-export default Test
