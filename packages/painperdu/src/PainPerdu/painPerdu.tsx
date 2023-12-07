ab33d610f6239e6833f7d1e526ec5811291b3cef
Kévin <kevin.joya@outlook.com>
2023-12-07 20:24:03 +0100

    Feat/cmd k launch (#8)

--- packages/painperdu/src/PainPerdu/painPerdu.tsx
+++ packages/painperdu/src/PainPerdu/painPerdu.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 export const PainPerdu = () => {
-  const [count, setCount] = React.useState(0);
+  const [isModal, setModal] = useState<boolean>(false);
 
-	function incrementCount() {
-		setCount(count + 1);
-	}
+	const handleEsc = (event: KeyboardEvent): void => {
+		const isMetaKey: boolean = event.metaKey
+		if (event.code === 'KeyK' && isMetaKey) setModal(true)
+	};
 
-  return (
+ useEffect(() => {
+	window.addEventListener('keydown', handleEsc);
+ })
+
+	return (
 		<section>
-			<h1> Ca part sur du pain perdu </h1>
-			<button
-				style={{ marginLeft: '10%' }}
-				onClick={incrementCount}
-			>
-				Go +1
-			</button>
-			<div style={{ textAlign: 'center' }}>
-				{ count }
-			</div>
+			{ isModal ? 'test' : '' }
 		</section>
-  );
+	);
 };
