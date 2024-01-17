67ab8ee8fc677fc8c08cdcf59e21c6cdbcf228d6
Kévin Joya <kevin.joya@outlook.com>
2024-01-17 15:37:32 +0100

    [feat/items_list] Deleted bg white && changed bg yellow

--- packages/painperdu/src/PainPerduListItem/PainPerduListItem.tsx
+++ packages/painperdu/src/PainPerduListItem/PainPerduListItem.tsx
@@ -16,7 +16,7 @@ export const PainPerduListItem: FC<Props> = ({ pathItem }) => {
 
   if (selectedPath === path.alias) {
     pathItem[index].isSelected = true
-    setPathClass('bg-yellow-300')
+    setPathClass('bg-yellow-200')
   }
  }
 
@@ -33,12 +33,12 @@ export const PainPerduListItem: FC<Props> = ({ pathItem }) => {
             return (
               <li
                 key={index}
-                className={`flex items-center ${item.isSelected ? pathClass : ''}`}
+                className={`flex items-center ${item.isSelected ? pathClass : 'bg-white'}`}
                 onMouseOver={() => { getSelectedPath(item, index) }}
                 onMouseLeave={() => { cleanSelectedPath(index) }}
               >
-                <a className="w-full bg-white border-solid shadow-none border-y pl-3 decoration-none">
-                  <div className="flex w-full items-center pr-2 h-14 bg-white text-neutral-500">
+                <a className="w-full border-solid shadow-none border-y pl-3 decoration-none">
+                  <div className="flex w-full items-center pr-2 h-14 text-neutral-500">
                     <div className="flex items-center h-14 pr-3">
                       <svg width="20" height="20" viewBox="0 0 20 20"><path d="M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4" stroke="currentColor" fill="none" fillRule="evenodd" strokeLinejoin="round"></path></svg>
                     </div>
