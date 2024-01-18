77243fb1d334f9ed2c90e3d985ab9a503f99dc83
Kévin Joya <kevin.joya@outlook.com>
2024-01-18 19:28:35 +0100

    [feat/items_list] Changed interface to type

--- packages/painperdu/src/types.ts
+++ packages/painperdu/src/types.ts
@@ -1,5 +1,8 @@
-export interface PathItem {
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type PathItem = {
 	alias: string
 	path: string
 	isSelected: boolean
 }
+
+export type { PathItem }
