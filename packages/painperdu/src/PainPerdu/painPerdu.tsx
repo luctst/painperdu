d40825ce4aa16bea91f7a5a40b6240c02f53c5e1
Kévin Joya <kevin.joya@outlook.com>
2024-01-25 15:06:42 +0100

    [feat/search_modal] Added create Portal logic && refacto code

--- packages/painperdu/src/PainPerdu/painPerdu.tsx
+++ packages/painperdu/src/PainPerdu/painPerdu.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react'
-import { PainPerduModal }  from '../PainPerduModal/PainPerduModal'
+import type { FC } from 'react'
+import { createPortal } from 'react-dom';
+import { PainPerduSearchBar } from '../components/PainPerduSearchBar'
+import { PainPerduFooter } from '../components/PainPerduFooter'
 import '../index.css'
 
-export const PainPerdu = () => {
+interface Props {
+  teleport: string
+}
+
+export const PainPerdu: FC<Props> = ({ teleport }) => {
   const [isModalActive, setModal] = useState<boolean>(false)
 
 	const handleEsc = (event: KeyboardEvent): void => {
@@ -22,21 +29,45 @@ export const PainPerdu = () => {
 		setModal(isModal)
 	}
 
- useEffect(() => {
-	window.addEventListener('keydown', handleEsc)
-	return () => {
-		window.removeEventListener('keydown', handleEsc)
-	}
- })
+	useEffect(() => {
+		window.addEventListener('keydown', handleEsc)
+		return () => {
+			window.removeEventListener('keydown', handleEsc)
+		}
+	})
 
-	return (
-		<section>
-			{
-				<PainPerduModal
-					showModal={isModalActive}
-					handleShowModal={shouldActiveModal}
-				/>
-			}
-		</section>
-	);
+ if (!isModalActive) return (null)
+
+ return createPortal(
+		<>
+			<div
+				className={`
+					flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50
+					outline-none focus:outline-none ${isModalActive ? 'bg-slate-500 opacity-80' : ''}`
+				}
+				onClick={() => { shouldActiveModal(false) }}
+			></div>
+			<div className="flex justify-center items-center my-0 mx-auto mt-40 overflow-x-hidden overflow-y-auto relative inset-0 z-50 outline-none focus:outline-none w-8/12">
+				<div className="border-0 rounded-xl shadow-lg relative flex flex-col bg-white outline-none focus:outline-none w-full">
+					<div className="bg-white px-0 pt-1">
+						<div>
+							<div>
+								<PainPerduSearchBar />
+								<main className="min-h-3 py-0 px-3 overflow-y-auto">
+									<div className="text-sm	my-0 mx-auto py-14 px-0 text-center w-4/5">
+										<p className="text-slate-500">Start writing to search routes</p>
+									</div>
+									<div>
+										<ul className="flex list-none	m-0 p-0"></ul>
+									</div>
+								</main>
+								<PainPerduFooter />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>,
+		document.querySelector(teleport) as HTMLElement
+ )
 };
